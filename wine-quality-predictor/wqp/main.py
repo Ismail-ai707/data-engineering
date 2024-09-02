2b91f951c6f3681de8781a098e702cb804919bb9
IsmailRhazi <ismail707rhazi@gmail.com>
2024-09-02 23:31:27 +0200

    copy wine-quality-predictor repo into data-engineering repo

---
+++ wine-quality-predictor/wqp/main.py
@@ -0,0 +1,5 @@
+from wqp.workflow import model_training_workflow
+
+if __name__ == '__main__':
+
+    model_training_workflow(data_path='http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv')
